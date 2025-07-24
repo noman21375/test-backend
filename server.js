eb5f0220712448f52f1c8945128d63531800b539
noman21375 <onlyupwork592@gmail.com>
2025-07-24 18:06:03 +0500

    adding cors and body-parcer

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.json());
 
 
 const PORT = process.env.PORT || 3601;
