a2912960f79cc8d2142e199ec4067386a1e30614
noman21375 <onlyupwork592@gmail.com>
2025-07-24 18:06:46 +0500

    adding routes

--- server.js
+++ server.js
@@ -5,6 +5,35 @@ const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.json());
 
+// welcome and goodbye routes
+app.post('/api/welcome', async (req, res) => {
+    try {
+         res.status(200).json({
+            success: true,
+            message: `${req.body.name} welcome to the server!`,
+        });
+    } catch (error) {
+        console.error('Payment status error:', error);
+        res.status(400).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
+app.post('/api/GoodBy', async (req, res) => {
+    try {
+         res.status(200).json({
+            success: true,
+            message: `${req.body.name} Goodbye from the server!`,
+        });
+    } catch (error) {
+        console.error('Payment status error:', error);
+        res.status(400).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
 
 const PORT = process.env.PORT || 3601;
 app.listen(PORT, () => {
